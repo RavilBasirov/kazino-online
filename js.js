126f92f8bafea5bcba8c0644836b9c8211749727
RavilBasirov <ekaapetr@gmal.com>
2024-09-21 12:34:22 +0300

    sigma

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 a = Math.floor((Math.random() * 7 + 1));
 b = Math.floor((Math.random() * 7 + 1));
 c = Math.floor((Math.random() * 7 + 1));
-let balance = 5000
-let stavka = 1000
-let vigrish
+let balance = 5000;
+let stavka = 1000;
+let vigrish = 0;
 
 // let result = [];
 // result[0] = 'visnya.jpg'
@@ -14,17 +14,19 @@ let vigrish
 // resolt[5] = 'dollar.jpg'
 // result[6] = 'seven.jpg'
 
-if (a == b, a == c, c == b) {
-    vigrish = 1000
+if (a == b || a == c || c == b) {
+    vigrish = stavka
 };
-if (a == b == c) {
+if (a == b & a == c & b ==c) {
     vigrish = stavka + stavka + stavka
 };
 
-if (a != b, a != c, c != b) {
+if (a != b & a != c & c != b) {
     vigrish = -stavka
 };
 
+balance = balance + vigrish
+
 console.log(a + " " + b + " " + c);
 
-console.log("Вы выйграли!" + vigrish + ", ваш баланс -", + balance);
+console.log("Вы выйграли" + vigrish + "!, ваш баланс -", + balance);
